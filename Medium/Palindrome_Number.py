b45502897759cef7fbde6ff29aa23c3bbf797330
Lironktf <Lironktf@gmail.com>
2025-09-17 23:36:02 -0400

    Completed palidrome number

---
+++ Medium/Palindrome_Number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+        reverse = str(x)[::-1]
+
+        if(str(x) == reverse):
+            return True
+        else:
+            return False
+        
